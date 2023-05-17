c70b27f9ac988130abda177b8fe86c95076ecaee
dev-damdam <dev.sodam@gmail.com>
2023-05-17 21:37:25 +0900

    :lipstick: Login 화면 배치 완료
    로그인/회원가입 화면에서 사용할 TemplateLoginForm.vue 구현

---
+++ src/templates/TemplateLoginForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <header></header>
+  <main class="w-full">
+    <section class="box">
+      <!-- icon -->
+      <img src="@/assets/icons/logo.png" class="w-5 block my-0 mx-auto" />
+      <article class="w-full flex flex-column gap-3">
+        <slot />
+      </article>
+    </section>
+    <section class="fixed right-0 w-5 z-1">
+      <img class="w-full" src="@/assets/imgs/computer.png" />
+      <Button
+        class="j2kb-notion-button"
+        label="J2KB Notion 바로가기"
+        severity="secondary"
+        outlined
+      />
+    </section>
+    <img class="fixed bottom-0 w-full" src="@/assets/imgs/footer.png" />
+  </main>
+</template>
+
+<script lang="ts" setup>
+import Button from "primevue/button";
+</script>
+
+<style lang="scss" scoped>
+.box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: rem(560px);
+
+  padding: rem(24px);
+
+  border-width: rem(1px);
+  border-style: solid;
+  border-color: var(--surface-border);
+  border-radius: var(--border-radius);
+
+  background-color: white;
+
+  z-index: 2;
+}
+
+.j2kb-notion-button {
+  position: absolute;
+  top: rem(100px);
+  right: rem(80px);
+  background-color: white !important;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,59 @@
+<template>
+  <TemplateLoginForm>
+    <p class="flex justify-content-center align-items-center gap-2 m-3">
+      <span>아직 회원이 아니신가요?</span>
+      <Button label="회원가입" severity="secondary" text class="p-0 h-2rem" />
+    </p>
+    <div class="flex flex-column gap-2">
+      <label for="email">Email</label>
+      <InputText
+        v-model="user.email"
+        id="email"
+        type="text"
+        placeholder="이메일을 입력해주세요"
+      />
+    </div>
+    <div class="flex flex-column gap-2">
+      <label for="password">Password</label>
+      <InputText
+        v-model="user.password"
+        id="password"
+        type="password"
+        placeholder="비밀번호를 입력해주세요"
+      />
+    </div>
+    <div class="flex justify-content-between align-items-center">
+      <p class="flex gap-2 align-items-center">
+        <Checkbox v-model="rememberEmail" inputId="chkbox1" binary />
+        <label for="chkbox1">이메일 저장</label>
+      </p>
+      <p class="flex align-items-center">
+        <Button
+          label="비밀번호 찾기"
+          severity="secondary"
+          text
+          class="p-0 h-2rem"
+        />
+      </p>
+    </div>
+    <Button label="로그인" class="w-full" size="small" />
+  </TemplateLoginForm>
+</template>
+
+<script lang="ts" setup>
+import { TUser } from "@/assets/models/TUser";
+import TemplateLoginForm from "@/templates/TemplateLoginForm.vue";
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+import Checkbox from "primevue/checkbox";
+import { ref } from "vue";
+
+const user = ref<Pick<TUser, "email" | "password">>({
+  email: "",
+  password: "",
+});
+
+const rememberEmail = ref<boolean>(false);
+</script>
+
+<style lang="scss" scoped></style>
