f1d0e764131a7c5354ffaf30a0d78cc385f96fc2
dev-damdam <dev.sodam@gmail.com>
2023-05-28 21:25:17 +0900

    :truck: BoardCard -> PostCard 으로 이름 변경
    기존 폴더명도 변경 board-card -> post-card

--- src/components/post-card/PostCard.vue
+++ src/components/post-card/PostCard.vue
@@ -1,42 +1,42 @@
 <template>
   <article class="board-card-wrapper">
-    <div class="flex mb-4 gap-3">
+    <div class="flex mb-4 gap-3 align-items-center">
       <img
         class="profile-image border-circle"
         src="https://plus.unsplash.com/premium_photo-1666264200739-caa822df28d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
       />
-      <p class="flex flex-column line-height-2 font-gray-900">
-        <span class="text-4xl">{{ props.name }}</span>
-        <span class="text-lg">{{ date }}</span>
+      <p class="flex align-items-center gap-2 line-height-2 font-gray-900">
+        <span class="text-2xl">{{ post.writer }}</span>
+        <span class="text-base">{{ date }}</span>
       </p>
     </div>
-    <p class="mb-3 title text-2xl font-bold font-black">
-      {{ props.title }}
-    </p>
     <p class="mb-3 content text-lg font-black">
-      {{ props.content }}
+      {{ post.content }}
     </p>
-    <p class="flex gap-2 align-items-center font-gray-900">
-      <i class="pi pi-comment text-2xl"></i>
-      <span class="text-2xl">{{ props.commentCount }}</span>
+    <p class="flex gap-7 align-items-center font-gray-900">
+      <span class="text-base">
+        <i class="pi pi-comment"></i>
+        {{ post.reply_ids.length }}
+      </span>
+      <span class="text-base">
+        <i class="pi pi-heart"></i>
+        {{ post.likes }}
+      </span>
     </p>
   </article>
 </template>
 <script lang="ts" setup>
+import { TPost } from "@/assets/models/TPost";
 import dayjs from "dayjs";
-import relativeTime from 'dayjs/plugin/relativeTime';
+import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
 
 const props = defineProps<{
-  name: string;
-  date: string;
-  title: string;
-  content: string;
-  commentCount: number;
+  post: TPost;
 }>();
 
-const date = dayjs(props.date).fromNow()
+const date = dayjs(props.post.created_at).fromNow();
 </script>
 <style lang="scss" scoped>
 .board-card-wrapper {
