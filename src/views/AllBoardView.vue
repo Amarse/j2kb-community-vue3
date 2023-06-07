eeaa78194412f2e727195411a66d26a33ea44945
Amarse <byicy08@gmail.com>
2023-06-08 02:18:43 +0900

    :hammer: Category 추가 하여 props 로 연결

--- src/views/AllBoardView.vue
+++ src/views/AllBoardView.vue
@@ -1,5 +1,5 @@
 <template>
-  <PostCard v-for="post in postList" :post="post"></PostCard>
+  <PostCard v-for="post,index in postList" :post="post" :key="index"></PostCard>
 </template>
 <script lang="ts" setup>
 import { TPost } from "@/assets/models/TPost";
@@ -8,6 +8,7 @@ import PostCard from "@/components/post/PostCard.vue";
 const postList: TPost[] = [
   {
     post_id: "test1",
+    category : "스터디/모임",
     writer: "박소담",
     content: `대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
       만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
@@ -23,6 +24,7 @@ const postList: TPost[] = [
   {
     post_id: "test2",
     writer: "박소담",
+    category : "개발 질문",
     content: `대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
       만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
       만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
