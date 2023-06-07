4964efb3b0a45f01cf7cd48886d0ad6797d70e88
Amarse <byicy08@gmail.com>
2023-06-08 02:20:52 +0900

    :lipstick: PostCard UI 배치 변경
    디자인 변경으로 인해 UI 배치 변경

--- src/components/post/PostCard.vue
+++ src/components/post/PostCard.vue
@@ -1,35 +1,45 @@
 <template>
-  <article class="board-card-wrapper" @click.prevent="goToDetailView(post.post_id)">
-    <div class="flex mb-4 gap-3 align-items-center">
-      <img
-        class="profile-image border-circle"
-        src="https://plus.unsplash.com/premium_photo-1666264200739-caa822df28d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
-      />
-      <p class="flex align-items-center gap-2 line-height-2 font-gray-900">
-        <span class="text-2xl">{{ post.writer }}</span>
-        <span class="text-base">{{ date }}</span>
+  <article
+    class="board-card-wrapper"
+    @click.prevent="goToDetailView(post.post_id)"
+  >
+    <div class="flex gap-3 align-items-top">
+      <p class="pr-2">
+        <img
+          class="profile-image border-circle"
+          src="https://plus.unsplash.com/premium_photo-1666264200739-caa822df28d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
+        />
       </p>
+      <div class="flex flex-column gap-3">
+        <div class="relative flex gap-2 line-height-3 font-gray-900">
+          <p class="flex flex-column">
+            <span class="text-sm">{{ post.category }}</span>
+            <span class="text-xl">{{ post.writer }}</span>
+          </p>
+          <span class="text-base pt-4">{{ date }}</span>
+        </div>
+        <p class="mb-3 content text-lg font-black">
+          {{ post.content }}
+        </p>
+        <p class="flex gap-5 align-items-center font-gray-900">
+          <span class="text-base">
+            <i class="pi pi-comment"></i>
+            {{ post.reply_ids.length }}
+          </span>
+          <span class="text-base">
+            <i class="pi pi-heart"></i>
+            {{ post.likes }}
+          </span>
+        </p>
+      </div>
     </div>
-    <p class="mb-3 content text-lg font-black">
-      {{ post.content }}
-    </p>
-    <p class="flex gap-5 align-items-center font-gray-900">
-      <span class="text-base">
-        <i class="pi pi-comment"></i>
-        {{ post.reply_ids.length }}
-      </span>
-      <span class="text-base">
-        <i class="pi pi-heart"></i>
-        {{ post.likes }}
-      </span>
-    </p>
   </article>
 </template>
 <script lang="ts" setup>
-import { TPost } from "@/assets/models/TPost";
-import router from "@/router";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
+import { TPost } from '@/assets/models/TPost';
+import router from '@/router';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 dayjs.extend(relativeTime);
 
@@ -43,22 +53,25 @@ const date = dayjs(props.post.created_at).fromNow();
 // methods
 const goToDetailView = (id: string) => {
   router.push(`/detail/${id}`);
-}
+};
 </script>
 <style lang="scss" scoped>
 .board-card-wrapper {
   padding: rem($card-padding);
-
+  border-bottom: rem(1px) solid #aaa;
   .title {
     @include text-truncate(1, 1.5);
   }
   .content {
     @include text-truncate(3, 1.5);
   }
-
-  &:nth-child(2n) {
-    // border-top: rem(1px) solid #aaa;
-    border-bottom: rem(1px) solid #aaa;
+  :deep(.pi) {
+    vertical-align: bottom;
   }
+
+  // &:nth-child(2n) {
+  //   // border-top: rem(1px) solid #aaa;
+  //   border-bottom: rem(1px) solid #aaa;
+  // }
 }
 </style>
