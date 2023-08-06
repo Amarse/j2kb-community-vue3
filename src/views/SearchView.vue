311638e9737d6540a10303346f10019c92e1efe8
dev-damdam <dev.sodam@gmail.com>
2023-08-06 20:45:51 +0900

    :sparkles: 게시글 검색 구현

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -22,187 +22,86 @@
       {{ message }}
     </p>
     <section v-else class="scroll-wrapper">
-      <PostList
-        v-for="post in postList"
-        :post="post"
-        @view="moveToPostDetailView"
-      />
+      <PostCard v-for="post in postList" :post="post" />
     </section>
   </main>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
+import { onBeforeUnmount, ref } from "vue";
 import { TPost } from "@/assets/models/TPost";
 import { computed } from "vue";
 import router from "@/router";
 import InputText from "primevue/inputtext";
-import PostList from "@/components/post/PostList.vue";
-
-const message = computed(() => {
-  if (isSearch.value) return "검색 결과를 찾을 수 없습니다.";
-  else return "검색어를 입력해주세요.";
-});
+import PostCard from "@/components/post/PostCard.vue";
+import FirebaseDatabase from "@/services/FirebaseDatabase";
 
 // variable
 const searchKeyword = ref<string>("");
-const isSearch = ref<boolean>(false);
+const message = ref<string>("");
 const postList = ref<TPost[]>([]);
+const database = ref<FirebaseDatabase | null>(null);
+
+// computed
+const resultMessage = computed({
+  get() {
+    return message.value;
+  },
+  set(value: string) {
+    message.value = value;
+  },
+});
+
+onBeforeUnmount(() => {
+  database.value = null;
+});
 
 // methods
+const init = () => {
+  resultMessage.value = "검색어를 입력해주세요.";
+  database.value = new FirebaseDatabase();
+};
+
 const back = () => {
   router.back();
 };
 
-const search = () => {
-  // todo something
-  // search post
-  isSearch.value = !isSearch.value;
+const search = async () => {
+  if (searchKeyword.value.length < 2) {
+    resultMessage.value = "2글자 이상 입력해주세요.";
+    return;
+  }
+  try {
+    const snapshot = await database.value?.queryEqualTo("posts", "content");
+    snapshot?.forEach((child) => {
+      if (child.val().content.includes(searchKeyword.value)) {
+        postList.value.push({
+          post_id: child.val().post_id,
+          category: child.val().category,
+          categoryKorean: child.val().categoryKorean,
+          email: child.val().email,
+          nickname: child.val().nickname,
+          content: child.val().content,
+          views: child.val().views,
+          likes: child.val().likes,
+          reply_ids:
+            child.val().reply_ids === undefined ? [] : child.val().reply_ids,
+          created_at: child.val().created_at,
+        });
+      }
+    });
 
-  // #test
-  postList.value = [
-    {
-      post_id: "test1",
-      nickname: "piou",
-      email: "test123456@test.com",
-      category: "study",
-      categoryKorean: "스터디/모임",
-      content: `대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.`,
-      views: 0,
-      likes: 0,
-      created_at: "2023-05-22 13:18",
-      reply_ids: [],
-    },
-    {
-      post_id: "test2",
-      nickname: "piou",
-      email: "test123456@test.com",
-      category: "study",
-      categoryKorean: "스터디/모임",
-      content: `대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.`,
-      views: 0,
-      likes: 0,
-      created_at: "2023-05-22 13:18",
-      reply_ids: [],
-    },
-    {
-      post_id: "test2",
-      nickname: "piou",
-      email: "test123456@test.com",
-      category: "study",
-      categoryKorean: "스터디/모임",
-      content: `대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.`,
-      views: 0,
-      likes: 0,
-      created_at: "2023-05-22 13:18",
-      reply_ids: [],
-    },
-    {
-      post_id: "test2",
-      nickname: "piou",
-      email: "test123456@test.com",
-      category: "study",
-      categoryKorean: "스터디/모임",
-      content: `대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.`,
-      views: 0,
-      likes: 0,
-      created_at: "2023-05-22 13:18",
-      reply_ids: [],
-    },
-    {
-      post_id: "test2",
-      nickname: "piou",
-      email: "test123456@test.com",
-      category: "study",
-      categoryKorean: "스터디/모임",
-      content: `대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.`,
-      views: 0,
-      likes: 0,
-      created_at: "2023-05-22 13:18",
-      reply_ids: [],
-    },
-    {
-      post_id: "test2",
-      nickname: "piou",
-      email: "test123456@test.com",
-      category: "study",
-      categoryKorean: "스터디/모임",
-      content: `대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.`,
-      views: 0,
-      likes: 0,
-      created_at: "2023-05-22 13:18",
-      reply_ids: [],
-    },
-    {
-      post_id: "test2",
-      nickname: "piou",
-      email: "test123456@test.com",
-      category: "study",
-      categoryKorean: "스터디/모임",
-      content: `대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.`,
-      views: 10000,
-      likes: 10000,
-      created_at: "2023-05-22 13:18",
-      reply_ids: [],
-    },
-    {
-      post_id: "test2",
-      nickname: "piou",
-      email: "test123456@test.com",
-      category: "study",
-      categoryKorean: "스터디/모임",
-      content: `대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.대나무 숲을
-      만들어보자.대나무 숲을 만들어보자.대나무 숲을 만들어보자.`,
-      views: 0,
-      likes: 0,
-      created_at: "2023-05-22 13:18",
-      reply_ids: [],
-    },
-  ];
+    postList.value.reverse();
+
+    if (postList.value.length === 0) {
+      resultMessage.value = "검색 결과를 찾을 수 없습니다.";
+    }
+  } catch (error: any) {
+    console.error(error);
+  }
 };
 
+init();
 // methods
-const moveToPostDetailView = (id: string) => {
-  router.push(`/detail/${id}`);
-};
 </script>
 <style lang="scss" scoped>
 :deep(.p-inputtext) {
